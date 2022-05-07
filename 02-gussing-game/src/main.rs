e63047fc3eaa80b9288e0fd34e833faaf2662840
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2022-05-07 23:00:14 +0430

    chapter2: gussing game

---
+++ 02-gussing-game/src/main.rs
@@ -0,0 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use colored::{self, Colorize};
+
+fn main() {
+    println!("Guess the a number between 1 and 10!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..11);
+    println!("secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please provide a valid number");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("{}","You win!!!".green());
+                break;
+            },
+            Ordering::Greater => println!("{}", "Too big".red()),
+            Ordering::Less => println!("{}","Too small".red()),
+        }
+    }
+}
